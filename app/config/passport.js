3be6503c8289c8453f83fffc8a35ae8613f807de
Patrick McClellan <drellik@hotmail.com>
2017-09-05 15:10:40 +0000

    changed login from github to facebook in passport.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var GitHubStrategy = require('passport-github').Strategy;
+/*var GitHubStrategy = require('passport-github').Strategy;
+var User = require('../models/users');
+var configAuth = require('./auth');
+*/
+
+var FacebookStrategy = require('passport-facebook').Strategy;
 var User = require('../models/users');
 var configAuth = require('./auth');
 
-module.exports = function (passport) {
+/*module.exports = function (passport) {
 	passport.serializeUser(function (user, done) {
 		done(null, user.id);
 	});
@@ -19,6 +24,53 @@ module.exports = function (passport) {
 		clientID: configAuth.githubAuth.clientID,
 		clientSecret: configAuth.githubAuth.clientSecret,
 		callbackURL: configAuth.githubAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({ 'github.id': profile.id }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+
+					newUser.github.id = profile.id;
+					newUser.github.username = profile.username;
+					newUser.github.displayName = profile.displayName;
+					newUser.github.publicRepos = profile._json.public_repos;
+					newUser.nbrClicks.clicks = 0;
+
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+};*/
+
+module.exports = function (passport) {
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new FacebookStrategy({
+		clientID: configAuth.facebookAuth.clientID,
+		clientSecret: configAuth.facebookAuth.clientSecret,
+		callbackURL: configAuth.facebookAuth.callbackURL
 	},
 	function (token, refreshToken, profile, done) {
 		process.nextTick(function () {
--- app/routes/index.js
+++ app/routes/index.js
@@ -49,6 +49,15 @@ module.exports = function (app, passport) {
 			successRedirect: '/',
 			failureRedirect: '/login'
 		}));
+		
+	app.route('/auth/facebook')
+	    .get(passport.authenticate('facebook'));
+	    
+	app.route('/auth/facebook/callback')
+	     .get(passport.authenticate('facebook', {
+	     	successRedirect: '/',
+	     	failureRedirect: '/login'
+	     }));
 
 	app.route('/api/:id/clicks')
 		.get(isLoggedIn, clickHandler.getClicks)
